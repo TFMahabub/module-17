a7d5164d6e379d7b48388e749d688ca7577fa84f
TFMahabub <rjmahabub543@gmail.com>
2022-08-01 16:29:19 +0600

    1st problem solved

--- practice-problem-1.js
+++ practice-problem-1.js
@@ -1,9 +1,15 @@
 var fruits = ["Apple", "Banana", "Orange"];
 
+//find the index num of banana
 var bananaIndexNumber = fruits.indexOf("Banana");
 
+//replase banana to mango
 fruits[1] = "Mango";
 
+//remove the last valu of the array
+fruits.pop("Orange");
 
+//add a last valu of the array
+fruits.push("Watermelon");
 
 console.log(fruits);
