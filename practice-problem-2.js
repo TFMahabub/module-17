768d5dda31d1a98fd4f401bc7b06dade2e4b89d0
TFMahabub <rjmahabub543@gmail.com>
2022-08-01 16:47:59 +0600

    2nd problem

---
+++ practice-problem-2.js
@@ -0,0 +1,21 @@
+var tomTotalScore = 66;
+
+if(tomTotalScore >= 39){
+
+  if(tomTotalScore >= 80){
+    console.log(A);
+  }
+  else if(tomTotalScore >= 60){
+    console.log(B);
+  }
+  else if(tomTotalScore >= 50){
+    console.log(C);
+  }
+  else if(tomTotalScore >= 40){
+    console.log(D);
+  }
+}
+
+else{
+  console.log(F);
+}
