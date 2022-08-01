54006e5391524defa3a49a19858f8aef52947124
TFMahabub <rjmahabub543@gmail.com>
2022-08-01 17:07:35 +0600

    3rd problem

--- practice-problem-2.js
+++ practice-problem-2.js
@@ -1,7 +1,13 @@
-var tomTotalScore = 66;
+
+// Main Marks
+var myNumbers = 85;
+// var tomsNumbers = 66;
+// var janesNumbers = 95;
+// var petersNumbers = 56;
+// var jhonsNumbers = 40;
 
 
-
+//conditions-
 if(tomTotalScore >= 39){
 
   if(tomTotalScore >= 80){
